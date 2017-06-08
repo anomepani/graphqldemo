6a166819410e6fdc1c71aff004857750f820d529
anomepani <arvindmepani@gmail.com>
2017-06-09 00:48:35 +0530

    Added basic type in graphql query

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ var schema = buildSchema(`
 rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
+
+// Construct a schema, using GraphQL schema language with different type
+/**
+ * The GraphQL schema language supports the scalar types of String, Int, Float, Boolean, and ID,
+ *  so you can use these directly in the schema you pass to buildSchema.
+    By default, every type is nullable - it's legitimate to return null as any of the scalar types.
+    Use an exclamation point to indicate a type cannot be nullable, so String! is a non-nullable string.
+    To use a list type, surround the type in square brackets, so [Int] is a list of integers.
+ */
+var basicTypeSchema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int],
+    hello: String,
+    name: String,
+rollDice(numDice: Int!, numSides: Int): [Int]
+  
+  }
+`);
 // // Created new query who accept dynamic paramter or variable
 // var rollDiceSchema = buildSchema(`
 // type Query {
@@ -35,12 +55,38 @@ var root = {
   }
 };
 
+var basicTypeRoot = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+   hello: () => {
+    return 'Hello world!';
+  },
+  name:()=>'Another Name',
+  rollDice: function ({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
+};
 // Commented below code and added express server code to execute graphql query
 // // Run the GraphQL query '{ hello }' and print out the response
 // graphql(schema, '{ hello,name }', root).then((response) => {
 //   console.log(response);
 // });     
 var app=express();
-app.use("/graphql",graphqlHTTP({schema:schema,rootValue:root,graphiql:true}));
+//// Commented static and old query and assigned new schema and root value
+//app.use("/graphql",graphqlHTTP({schema:schema,rootValue:root,graphiql:true}));
+
+app.use("/graphql",graphqlHTTP({schema:basicTypeSchema,rootValue:basicTypeRoot,graphiql:true}));
+
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
