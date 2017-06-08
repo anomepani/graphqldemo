3d92b3928f4afc38acb4de30e80a3957095b99e0
anomepani <arvindmepani@gmail.com>
2017-06-09 00:13:19 +0530

    Created new query which accept variable with multiple arguments

--- server.js
+++ server.js
@@ -7,17 +7,32 @@ var graphqlHTTP = require('express-graphql');
 var schema = buildSchema(`
   type Query {
     hello: String,
-    name: String
+    name: String,
+rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
+// // Created new query who accept dynamic paramter or variable
+// var rollDiceSchema = buildSchema(`
+// type Query {
+// rollDice(numDice: Int!, numSides: Int): [Int]
+
+// }`);
 
 //console.log(schema);
 // The root provides a resolver function for each API endpoint
+// Added rollDice function or property to resolve with dynamic value
 var root = {
   hello: () => {
     return 'Hello world!';
   },
-  name:()=>'Another Name'
+  name:()=>'Another Name',
+  rollDice: function ({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
 };
 
 // Commented below code and added express server code to execute graphql query
