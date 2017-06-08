941d9e3c3df1103983eb571a3c2504046c35cb42
anomepani <arvindmepani@gmail.com>
2017-06-08 23:53:17 +0530

    Added Express server ith graphql

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var { graphql, buildSchema } = require('graphql');
+//Add express and express-graphql package to create server
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -17,7 +20,12 @@ var root = {
   name:()=>'Another Name'
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello,name }', root).then((response) => {
-  console.log(response);
-});     
+// Commented below code and added express server code to execute graphql query
+// // Run the GraphQL query '{ hello }' and print out the response
+// graphql(schema, '{ hello,name }', root).then((response) => {
+//   console.log(response);
+// });     
+var app=express();
+app.use("/graphql",graphqlHTTP({schema:schema,rootValue:root,graphiql:true}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
